e61f7d776658f8911b6555df8876fde387c42db6
Chen HT <mongol.huhhot@gmail.com>
2026-07-14 19:19:05 +0900

    更新。陳

--- packed/generic-master-private/src/main.js
+++ packed/generic-master-private/src/main.js
@@ -3,7 +3,7 @@ import { createApp, h } from 'vue'
 import { createPinia } from 'pinia'
 import wrapper from 'vue3-webcomponent-wrapper'
 import WebComponent from '@/components/top-page.ce.vue'
-
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 
@@ -13,6 +13,20 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { VDateInput } from 'vuetify/labs/VDateInput'
 import ImageUploader from '@/components/forms/fields/ImageUploader.vue'
+
+const myCustomTheme = {
+    dark: false,
+    colors: {
+        primary: '#00A0E9',     // Replace with your desired primary color
+        secondary: '#0068B7',   // Replace with your desired secondary color
+        accent: '#8c9eff',      // Replace with your desired accent color
+        success: '#00B9EF',     // Replace with your desired success color
+        warning: '#6E60A8',     // Replace with your desired warning color
+        error: '#E6003E',       // Replace with your desired error color
+        info: '#003894',        // Replace with your desired info color
+    },
+};
+
 
 const customJa = {
   ...ja,
@@ -35,7 +49,19 @@ const customJa = {
 const vuetify = createVuetify({
   components: { ...components, VDateInput,ImageUploader },
   directives,
-
+  theme: {
+    defaultTheme: 'myCustomTheme',
+    themes: {
+      myCustomTheme,
+    },
+  },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
   locale: {
     locale: 'ja',
     fallback: 'en',
